87bbe5407d1085b60e648feb82aa0370ebd7a790
Urinboev Abdushukur <abdushukururinboev@gmail.com>
2020-12-21 15:59:28 +0500

    React Forms

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+
+function App() {
+
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("")
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick(event){
+    setHeadingText(name);
+
+    event.preventDefault();
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+      <input type="text" placeholder="What's your name?" onChange={handleChange}/>
+      <button type="submit">Submit</button>
+    </form>
+    </div>
+  );
+}
+
+export default App;
